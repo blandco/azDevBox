192a66cf23bd07d9902ab4fea654e91169ae067f
blandco <jbland@gmail.com>
2020-08-13 15:59:55 -0500

    Adds script for Azure VM extensions.

---
+++ post-deploy.ps1
@@ -0,0 +1,6 @@
+# // Bootstrap choco
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+# // Blindly accept legal terms
+choco feature enable -n allowGlobalConfirmation
+# // Do work
+choco install googlechrome
